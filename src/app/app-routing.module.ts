88786ba16ef603dd282aecd3cc5b45f068b7d790
Victor <olaitanvictor99@gmail.com>
2020-03-27 22:24:50 -0400

    Landing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingComponent } from "./landing/landing.component";
+import { PeopleComponent } from "./people/people.component";
+import { SocietyComponent } from "./society/society.component";
+import { QuizComponent } from "./quiz/quiz.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "people", component: PeopleComponent },
+  { path: "society", component: SocietyComponent },
+  { path: "quiz", component: QuizComponent },
+  { path: "", component: LandingComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PeopleComponent } from './people/people.component';
+import { LandingComponent } from './landing/landing.component';
+import { SocietyComponent } from './society/society.component';
+import { QuizComponent } from './quiz/quiz.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PeopleComponent,
+    LandingComponent,
+    SocietyComponent,
+    QuizComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
