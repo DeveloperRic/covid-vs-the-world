2f9dcbbad4dc8d803a336da20ed4020e5da84425
gadzyCode <45364086+gadzyCode@users.noreply.github.com>
2020-03-30 04:13:30 -0400

    final

--- src/app/help/help.component.ts
+++ src/app/help/help.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-help',
@@ -9,11 +10,16 @@ import { Title } from '@angular/platform-browser';
 export class HelpComponent implements OnInit {
 
   constructor(
-    private titleService: Title
+    private titleService: Title,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
     this.titleService.setTitle("What you can do to help | covid-vs-the-world");
+  }
+
+  goBack() {
+    this.router.navigate(["/"]);
   }
 
 }
