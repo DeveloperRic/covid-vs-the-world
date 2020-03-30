49a39099347bc2f45a30ffee243637333b2f0a31
gadzyCode <45364086+gadzyCode@users.noreply.github.com>
2020-03-29 23:31:40 -0400

    quiz

---
+++ src/app/quiz/questions.ts
@@ -0,0 +1,88 @@
+export const questions = [
+  {
+    question: 'How is the corona virus spread?',
+    answer: 'Person to person',
+    explanation:
+      'Covid19 is transmitted through Person to Person conflict',
+    false_answers: ['Airbourne', 'Waterbourne', 'Bloodbourne']
+  },
+  {
+    question: 'How did the corona virus start?',
+    answer: 'Wet Market in Wuhan',
+    explanation:
+      'Sources say that the virus came from a "Wet Market" in Wuhan which sold both dead and live animals',
+    false_answers: [
+      'Okavango Delta National Park, Africa',
+      'Lake Natron, Africa',
+      'Darien Gap - Colombia'
+    ]
+  },
+  {
+    question: 'Which country has been effected most?',
+    answer: 'USA',
+    explanation:
+      '"In the United States, at least 81,321 people are known to have been infected with the coronavirus, including more than 1,000 deaths — more cases than China, Italy or any other country has seen, according to data gathered by The New York Times." - Donald G. Mcneil Jr',
+    false_answers: ['China', 'Canada', 'Africa']
+  },
+  {
+    question:
+      'What should you **NOT** do in order to prevent yourself from getting Covid19?',
+    answer: 'Not take the precautions from governments and hospitals seriously',
+    explanation:
+      'Between people who are in close contact with one another (within about 6 feet).',
+    false_answers: [
+      'Hang with friends',
+      'Go to a bar',
+      'Dont listen to what the government and hospitals'
+    ]
+  },
+  {
+    question:
+      'How long Should you self isolate if you think you contain the virus?',
+    answer: '14 days',
+    explanation:
+      'stay at home and monitor yourself for symptoms, even if mild, for 14 days',
+    false_answers: ['1 day', '5 days', '10 days']
+  },
+  {
+    question: 'Which groups are most at risk?',
+    answer: 'Elderly and people with asthma or weak immune systems',
+    explanation:
+      'Older adults and people of any age who have serious underlying medical conditions, like asthma, may be at higher risk for more serious complications from COVID-19.',
+    false_answers: ['Millennials', 'Children', 'Middle Aged People']
+  },
+  {
+    question: 'What has the corona virus effected',
+    answer: 'All answers',
+    explanation: 'Economies in every countries have been falling at a tremendous pace, Buisnesses getting forced to shut down, Schools closing, and the Trade collapsing',
+    false_answers: ['Buisnesses', 'Schools', 'Trade']
+  },
+  {
+    question: 'What are the signs and symptoms?',
+    answer: 'Fever, Cough, Shortness of breath',
+    explanation: 'Fever, Cough, Shortness of breath are symptoms and signs of Covid19',
+    false_answers: [
+        "Running Nose",
+        "Sleepy",
+        "Depression"
+    ]
+  },{
+    question: 'Which one of these people can spread the virus?',
+    answer: 'People who currently have the virus',
+    explanation: 'People infected are the people who can spread the virus, not people who have been through quarantine or have been released from isolation',
+    false_answers: [
+        "People who have came out of isolation",
+        "People who have had the virus previously and have recovered",
+        "People who don't have the virus"
+    ]
+  },{
+    question: 'What ethnicities get effected the most',
+    answer: 'Person of any ethnicity or race',
+    explanation: 'Person of any ethnicity or race can get the virus, all should follow precautions set by the government and health care facilities',
+    false_answers: [
+        "Asians",
+        "Whites",
+        "African's"
+    ]
+  }
+];
--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
 import { Router } from '@angular/router';
 import { Title } from '@angular/platform-browser';
+import { questions } from './questions';
 
 @Component({
   selector: 'app-quiz',
@@ -8,19 +9,100 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./quiz.component.css']
 })
 export class QuizComponent implements OnInit {
-  back_nav = "Home";
+  private questions = questions;
+  back_nav = 'Home';
+  displayQuestions = {
+    qOneText: '',
+    qTwoText: '',
+    qThreeText: '',
+    qFourText: ''
+  };
+  currentQuestion = 0;
+  mark;
+  answer;
+  question = '';
+  quizCompleted = false;
+  questionCompleted = false;
 
-  constructor(
-    private titleService: Title,
-    private router: Router
-  ) { }
+  score = 0;
+
+  constructor(private titleService: Title, private router: Router) {}
 
   ngOnInit(): void {
-    this.titleService.setTitle("Quiz | covid-vs-the-world");
+    this.titleService.setTitle('Quiz | covid-vs-the-world');
+    this.nextQuestion();
+  }
+
+  answerSelected(num){
+    this.questionCompleted = true;
+    this.answer = questions[this.currentQuestion-1].explanation;
+    if(this.checkQuestion(num)){
+      this.mark = "Correct!"
+    }
+    else{
+      this.mark = "Incorrect";
+    }
+  }
+
+  nextQuestion() {
+    this.questionCompleted = false;
+    if (this.currentQuestion == 10) {
+      this.quizCompleted = true;
+    }
+    else if(!this.quizCompleted) {
+      this.question = questions[this.currentQuestion].question;
+      let randomizedAnswers = this.getAnswers();
+      this.displayQuestions.qOneText = randomizedAnswers[0];
+      this.displayQuestions.qTwoText = randomizedAnswers[1];
+      this.displayQuestions.qThreeText = randomizedAnswers[2];
+      this.displayQuestions.qFourText = randomizedAnswers[3];
+      this.currentQuestion++;
+    }
+  }
+
+  checkQuestion(num) {
+    if (questions[this.currentQuestion - 1].answer == this.getDisplayQuestionText(num)){
+      this.score++;
+      return true;
+    }
+    else
+     return false;
+  }
+
+  getAnswers() {
+    let array = questions[this.currentQuestion].false_answers;
+    array.push(questions[this.currentQuestion].answer);
+    return this.shuffle(array);
   }
 
   goBack() {
-    this.router.navigate(["/"]);
+    this.router.navigate(['/']);
   }
 
+  shuffle(array) {
+    var currentIndex = array.length,
+      temporaryValue,
+      randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+  }
+
+  getDisplayQuestionText(num){
+    if(num == 0)return this.displayQuestions.qOneText
+    else if (num == 1) return this.displayQuestions.qTwoText
+    else if (num == 2) return this.displayQuestions.qThreeText
+    else return this.displayQuestions.qFourText
+  }
 }
