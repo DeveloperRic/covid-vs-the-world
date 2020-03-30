f7c798291e0dfea8b17fd8cbda0cf8389aa75430
Victor <olaitanvictor99@gmail.com>
2020-03-30 00:33:08 -0400

    ready for deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  STITCH_CLIENT_APP_ID: "covid-vs-the-world-avzes",
+  STITCH_REDIRECT_URL: "https://covidvstheworld.online/login/redirect",
+  MONGODB_DATABASE_NAME: "prod"
 };
